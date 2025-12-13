80c6486d6390858a9f075212f884f9386ecdf963
Steven Kalt <kalt.steven@gmail.com>
2025-12-13 16:26:17 -0500

    feat: listen for changes in dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,35 +4,46 @@
 	import { layers, namedFlavor } from '@protomaps/basemaps';
 	import 'maplibre-gl/dist/maplibre-gl.css';
 	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 	const PHILA_URL = 'https://yra7vsjs9nrw0ud7.public.blob.vercel-storage.com/philadelphia.pmtiles';
 	const proto = new Protocol();
-	// TODO: mutable store of map
 	maplibre.addProtocol('pmtiles', proto.tile);
-
+	const darkTheme = writable(false);
+	let _map = writable<maplibre.Map | null>(null);
+	const style = (dark: boolean): maplibre.StyleSpecification => ({
+		version: 8,
+		sources: {
+			protomaps: {
+				type: 'vector',
+				url: 'pmtiles://' + PHILA_URL
+			}
+		},
+		sprite: 'https://protomaps.github.io/basemaps-assets/sprites/v4/white',
+		glyphs: 'https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf',
+		layers: layers('protomaps', namedFlavor(dark ? 'black' : 'white'), { lang: 'en' })
+	});
 	onMount(() => {
-		console.clear();
+		{
+			// listen for system color preference changes
+			// technique from https://robkendal.co.uk/blog/2024-11-21-detecting-os-level-dark-mode/
+			const _darkMode = window.matchMedia('(prefers-color-scheme: dark)');
+			darkTheme.set(_darkMode.matches);
+			_darkMode.addEventListener('change', (e) => darkTheme.set(e.matches));
+		}
 		const config: maplibre.MapOptions = {
 			container: 'map',
-			center: { lng: 39.96373852937114, lat: -75.25713708454445 },
+			center: { lat: 39.96373852937114, lng: -75.25713708454445 },
 			zoom: 10.74,
-			hash: true,
-			style: {
-				version: 8,
-				sources: {
-					protomaps: {
-						type: 'vector',
-						url: 'pmtiles://' + PHILA_URL
-					}
-				},
-				sprite: 'https://protomaps.github.io/basemaps-assets/sprites/v4/white',
-				glyphs: 'https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf',
-				layers: layers('protomaps', namedFlavor('light'), { lang: 'en' })
-			}
+			// hash: true,
+			style: style($darkTheme)
 		};
-
-		console.log(config);
 		const m = new maplibre.Map(config);
-		console.log(m);
+		_map.set(m);
+		console.log();
+	});
+	darkTheme.subscribe((dark) => {
+		let m = $_map;
+		if (m?.style.setState(style(dark))) m.redraw();
 	});
 </script>
 
