dc78375c39b473ebe7962950de0184e74b8b5247
Steven Kalt <kalt.steven@gmail.com>
2025-12-13 17:42:45 -0500

    feat: add info-bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import 'maplibre-gl/dist/maplibre-gl.css';
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
+	// FIXME: move to r2
 	const PHILA_URL = 'https://yra7vsjs9nrw0ud7.public.blob.vercel-storage.com/philadelphia.pmtiles';
 	const proto = new Protocol();
 	maplibre.addProtocol('pmtiles', proto.tile);
@@ -38,14 +39,70 @@
 			style: style($darkTheme)
 		};
 		const m = new maplibre.Map(config);
+		m.on('error', (e) => {
+			console.log(e);
+		});
 		_map.set(m);
-		console.log();
+		// TODO: register click callbacks
 	});
 	darkTheme.subscribe((dark) => {
 		let m = $_map;
-		if (m?.style.setState(style(dark))) m.redraw();
+		if (m?.style.setState(style(dark))) {
+			m.redraw(); // FIXME: doesn't work
+			m.setZoom(m.getZoom() + 0.01); // yet this does
+		}
 	});
 </script>
 
-<h1>Happy Holidays from Philadelphia, PA!</h1>
-<div id="map" style="height: 90vh; width: 100vw"></div>
+<div id="box">
+	<h1>Happy Holidays from Philadelphia, PA!</h1>
+	<div id="container">
+		<div id="map"></div>
+		<div id="info">
+			info
+			<!-- infobar -->
+		</div>
+	</div>
+</div>
+
+<style>
+	#box {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+	h1 {
+		text-align: center;
+		flex: 0 1 auto;
+	}
+	/* https://m2.material.io/design/layout/responsive-layout-grid.html#breakpoints */
+	#container {
+		flex: 1 1 auto;
+		display: flex;
+		height: 100%;
+		/* --max-height:  */
+	}
+	#info {
+		margin: 0 1rem;
+		border: 1px solid #eee;
+	}
+	#map,
+	#info {
+		width: 50%;
+		height: 100%;
+		/* height: 90vh; */
+	}
+	@media (max-width: 600px) {
+		#container {
+			display: block;
+		}
+		#info {
+			margin: 1rem 0;
+		}
+		#map,
+		#info {
+			width: 100%;
+			height: 50%;
+		}
+	}
+</style>
